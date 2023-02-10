92d84be5c052742995c7c311d38186124d9ca9d8
sumeetacewebx <sumeet@acewebx.com>
2023-02-11 00:08:57 +0530

    add

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -35,6 +35,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
     return (
       <>
+       <ToastContainer />
         <AuthContextProvider>
           <ProtectedRoute>{renderApp()}</ProtectedRoute>
         </AuthContextProvider>
